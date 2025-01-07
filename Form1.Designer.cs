6045e159ccd0c6f304eca252e5b41ede193653e7
Pierre CHAUVEAU <pierre53260@hotmail.fr>
2025-01-07 08:40:33 +0100

    Ajoutez des fichiers projet.

---
+++ Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace AccesHemiCycle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.pictureBoxCamera = new System.Windows.Forms.PictureBox();
+            this.comboBoxCamera = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonRead = new System.Windows.Forms.Button();
+            this.textBoxContenu = new System.Windows.Forms.TextBox();
+            this.timerCamera = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCamera)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(565, 81);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(160, 41);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            // 
+            // pictureBoxCamera
+            // 
+            this.pictureBoxCamera.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBoxCamera.Location = new System.Drawing.Point(27, 81);
+            this.pictureBoxCamera.Name = "pictureBoxCamera";
+            this.pictureBoxCamera.Size = new System.Drawing.Size(511, 464);
+            this.pictureBoxCamera.TabIndex = 1;
+            this.pictureBoxCamera.TabStop = false;
+            // 
+            // comboBoxCamera
+            // 
+            this.comboBoxCamera.FormattingEnabled = true;
+            this.comboBoxCamera.Location = new System.Drawing.Point(182, 30);
+            this.comboBoxCamera.Name = "comboBoxCamera";
+            this.comboBoxCamera.Size = new System.Drawing.Size(271, 21);
+            this.comboBoxCamera.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Camera";
+            // 
+            // buttonRead
+            // 
+            this.buttonRead.Location = new System.Drawing.Point(768, 81);
+            this.buttonRead.Name = "buttonRead";
+            this.buttonRead.Size = new System.Drawing.Size(160, 41);
+            this.buttonRead.TabIndex = 4;
+            this.buttonRead.Text = "Read";
+            this.buttonRead.UseVisualStyleBackColor = true;
+            // 
+            // textBoxContenu
+            // 
+            this.textBoxContenu.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxContenu.Location = new System.Drawing.Point(582, 161);
+            this.textBoxContenu.Multiline = true;
+            this.textBoxContenu.Name = "textBoxContenu";
+            this.textBoxContenu.Size = new System.Drawing.Size(335, 140);
+            this.textBoxContenu.TabIndex = 5;
+            // 
+            // timerCamera
+            // 
+            this.timerCamera.Tick += new System.EventHandler(this.timerCamera_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(982, 589);
+            this.Controls.Add(this.textBoxContenu);
+            this.Controls.Add(this.buttonRead);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBoxCamera);
+            this.Controls.Add(this.pictureBoxCamera);
+            this.Controls.Add(this.buttonStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCamera)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.PictureBox pictureBoxCamera;
+        private System.Windows.Forms.ComboBox comboBoxCamera;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonRead;
+        private System.Windows.Forms.TextBox textBoxContenu;
+        private System.Windows.Forms.Timer timerCamera;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using ZXing;
+using ZXing.Aztec;
+
+namespace AccesHemiCycle
+{
+    public partial class Form1 : Form
+    {
+        private FilterInfoCollection captureDevice;
+        private VideoCaptureDevice finalVideo;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            captureDevice = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo Device in captureDevice) { comboBoxCamera.Items.Add(Device.Name); }
+
+            comboBoxCamera.SelectedIndex = 0;
+            finalVideo = new VideoCaptureDevice();
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            finalVideo = new VideoCaptureDevice(captureDevice[comboBoxCamera.SelectedIndex].MonikerString);
+            finalVideo.NewFrame += new NewFrameEventHandler(FinalVideo_NewFrame);
+            finalVideo.Start();
+        }
+
+        private void FinalVideo_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            pictureBoxCamera.Image = (Bitmap)eventArgs.Frame.Clone();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (finalVideo.IsRunning == true) { finalVideo.Stop(); }
+        }
+
+        private void timerCamera_Tick(object sender, EventArgs e)
+        {
+            BarcodeReader reader = new BarcodeReader();
+            Result result = reader.Decode((Bitmap)pictureBoxCamera.Image);
+
+            try
+            {
+                string decoded = result.ToString().Trim();
+                if (decoded != "")
+                {
+                    textBoxContenu.Text = decoded;
+                }
+            }
+            catch (Exception ex)
+            {
+            
+            }
+        }
+    }
+}
